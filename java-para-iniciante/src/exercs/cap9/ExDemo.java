99ece49fca0049b0210686d9d3dc3abee5e2d37c
Mony Machado <monymachad@gmail.com>
2025-06-02 19:40:04 -0300

    add cap9 Tratamento de Exceçções

---
+++ java-para-iniciante/src/exercs/cap9/ExDemo.java
@@ -0,0 +1,15 @@
+package exercs.cap9;
+/*
+@author: Simone (Grupo de Estudos sobre Java)
+@version: 1.0
+@since: 03/05/2025 (Dt de quando implementei essa classe no GE)
+@Artefato: ExDemo.java
+Atividades do capítulo 9 - Tratamento de Exceções
+Livro: Java para Iniciantes - Herbert Schildt
+@description:  Exemplificando o conceito de Tratamento de Exceções.
+                Esta é a classe ExDemo, que demonstra o uso de tratamento de exceções em Java.
+                Métodos: Não possui métodos próprios, mas pode conter métodos para demonstrar o tratamento de exceções.
+                Atributos: Não possui atributos próprios, mas pode ser usada para encapsular lógica de tratamento de exceções.
+*/
+public class ExDemo {
+}
