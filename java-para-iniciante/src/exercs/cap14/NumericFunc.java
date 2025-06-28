0889414043d480414bd4c91ab668d8c5848d9b16
apinheira-tech <1822533@aluno.univesp.br>
2025-06-28 13:37:07 -0300

    Cap14: Exp Lambda E Ref de metodo - Excercicio pg 476

--- java-para-iniciante/src/exercs/cap14/NumericFunc.java
+++ java-para-iniciante/src/exercs/cap14/NumericFunc.java
@@ -1,21 +1,35 @@
 package exercs.cap14;
 
+/*
+Expressão Lambda de bloco
+Uma lambda de bloco que encontra o menor fator positivo de um valor inteiro.
+de um valor inteiro.
+ */
 public class NumericFunc {
     public static void main(String[] args) {
-        // Exemplo de uso da função isEven
-        System.out.println("Is 4 even? " + isEven(4)); // true
-        System.out.println("Is 5 even? " + isEven(5)); // false
+        // Esta lambda de bloco retorna o menor fator positivo de um valor
+        NumericFuncDefault smallestFactor = (n) -> {
+            int result = 1;
+            // Obtem o valor absoluto de n
+            // n = Math.abs(n);
+            n = n < 0 ? -n : n; // Se n for negativo, torna-o positivo
+            for (int i = 2; i <= n / i; i++) {
+                if (n % i == 0) {
+                    result = i;
+                    break;
+                }
+            }
+            // return result == 0 ? n : result;
+            return result;
+        };
 
-        // Exemplo de uso da função isOdd
-        System.out.println("Is 3 odd? " + isOdd(3)); // true
-        System.out.println("Is 6 odd? " + isOdd(6)); // false
-    }
+        System.out.println("Menor fator positivo de 12: " + smallestFactor.func(12));
+        System.out.println("Menor fator positivo de 11: " + smallestFactor.func(11));
 
-    public static boolean isEven(int number) {
-        return number % 2 == 0;
-    }
 
-    public static boolean isOdd(int number) {
-        return number % 2 != 0;
     }
+}
+interface NumericFuncDefault  {
+    // A interface funcional deve ter um único método abstrato
+    int func(int n);
 }
